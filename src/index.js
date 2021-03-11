f85addee8148b2b5215eb7819b894fd65d5658ce
Nastya Kazakovaa <nasty.nagato@gmail.com>
2021-03-11 22:06:19 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+exports.min = function min(array) {
+    if (array && array.length !== 0) {
+        let min = array[0];
+        for (let i = 0; i <= array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            } else {
+                min = min;
+            }
+        }
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length !== 0) {
+        let max = array[0];
+        for (let i = 0; i <= array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            } else {
+                max = max;
+            }
+        }
+        return max;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length !== 0) {
+        let avg = 0;
+        for (let i = 0; i < array.length; i++) {
+            avg = avg + array[i];
+        }
+        return avg / array.length;
+    } else {
+        return 0;
+    }
+};
